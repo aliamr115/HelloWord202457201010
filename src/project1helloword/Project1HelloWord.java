1baae85d1075bb6b8ccec96bcbae4f9f12a3cf85
aliamr <aliamasrukha@gmail.com>
2025-03-19 10:44:07 +0700

    first commit

---
+++ src/project1helloword/Project1HelloWord.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package project1helloword;
+
+/**
+ *
+ * @author user
+ */
+public class Project1HelloWord {
+/*NAMA : ALIA MASRUKHATUR RIZKIYAH
+  NIM  : 202457201010
+  PRODI: SISTEM INFORMASI A
+    */
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        System.out.println("It's me Alia");
+    }
+    
+}
